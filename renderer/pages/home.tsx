5757175da154b09c83fd1b3bc4182f7d8f239036
Sebastian Velez Galeano <sebastian.velez@avenfor.com>
2023-08-21 12:02:40 -0500

    login and signup

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,24 +1,95 @@
-import React from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
+import React, { useEffect, useRef } from "react";
 
 function Home() {
+  const signUpButtonRef = useRef(null);
+  const signInButtonRef = useRef(null);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const signUpButton = signUpButtonRef.current;
+    const signInButton = signInButtonRef.current;
+    const container = containerRef.current;
+
+    const handleSignUpClick = () =>
+      container.classList.add("right-panel-active");
+    const handleSignInClick = () =>
+      container.classList.remove("right-panel-active");
+
+    signUpButton.addEventListener("click", handleSignUpClick);
+    signInButton.addEventListener("click", handleSignInClick);
+
+    return () => {
+      signUpButton.removeEventListener("click", handleSignUpClick);
+      signInButton.removeEventListener("click", handleSignInClick);
+    };
+  }, []);
+
   return (
-    <React.Fragment>
-      <Head>
-        <title>Home - Nextron (with-typescript)</title>
-      </Head>
-      <div>
-        <p>
-          ⚡ Electron + Next.js ⚡ -
-          <Link href="/next">
-            <a>Go to next page</a>
-          </Link>
-        </p>
-        <img src="/images/logo.png" />
+    <div className="container" ref={containerRef}>
+      <div className="form-container sign-up-container">
+        <form action="#">
+          <h1>Create Account</h1>
+          <div className="social-container">
+            <a href="#" className="social">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" className="social">
+              <i className="fab fa-google-plus-g"></i>
+            </a>
+            <a href="#" className="social">
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+          </div>
+          <span>or use your email for registration</span>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign Up</button>
+        </form>
       </div>
-    </React.Fragment>
+      <div className="form-container sign-in-container">
+        <form action="#">
+          <h1>Sign in</h1>
+          <div className="social-container">
+            <a href="#" className="social">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" className="social">
+              <i className="fab fa-google-plus-g"></i>
+            </a>
+            <a href="#" className="social">
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+          </div>
+          <span>or use your account</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forgot your password?</a>
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h1>Welcome Back!</h1>
+            <p>
+              To keep connected with us please login with your personal info
+            </p>
+            <button className="ghost" ref={signInButtonRef}>
+              Sign In
+            </button>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1>Hello, Friend!</h1>
+            <p>Enter your personal details and start journey with us</p>
+            <button className="ghost" ref={signUpButtonRef}>
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-};
+}
 
 export default Home;
