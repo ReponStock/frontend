fc0074bfe1743ec376ea111a773ccbbdbd76eae8
Miguel C <miguel.cabrera@gestionpositiva.com>
2023-09-20 15:03:53 -0500

    simulated login for demo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,43 +1,45 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
-import './styles.css'
+import { useRef, useState } from "react";
+import { useRouter } from "next/navigation"; // Importa el hook useRouter
+import './styles.css';
 
 export default function Login() {
   const signUpButtonRef = useRef<HTMLButtonElement | null>(null);
   const signInButtonRef = useRef<HTMLButtonElement | null>(null);
   const containerRef = useRef<HTMLDivElement | null>(null);
+  const [isSignUp, setIsSignUp] = useState(false);
+  const router = useRouter(); // Inicializa el hook useRouter
 
-  useEffect(() => {
-    const signUpButton = signUpButtonRef.current;
-    const signInButton = signInButtonRef.current;
-    const container = containerRef.current;
+  const handleSignUpClick = () => {
+    containerRef?.current?.classList.add("right-panel-active");
+    setIsSignUp(true);
+  };
 
-    const handleSignUpClick = () => {
-      // Comprobar si el contenedor existe antes de agregar la clase
-      container?.classList.add("right-panel-active");
-    };
+  const handleSignInClick = () => {
+    containerRef?.current?.classList.remove("right-panel-active");
+    setIsSignUp(false);
+  };
 
-    const handleSignInClick = () => {
-      // Comprobar si el contenedor existe antes de eliminar la clase
-      container?.classList.remove("right-panel-active");
-    };
+  const handleSignIn = () => {
+    // Tu lógica de inicio de sesión aquí
 
-    // Asegurarse de que los elementos existan antes de agregar los event listeners
-    signUpButton?.addEventListener("click", handleSignUpClick);
-    signInButton?.addEventListener("click", handleSignInClick);
+    // Después del inicio de sesión exitoso, redirige a la página principal
+    
+    router.push("/filesUpload");
+  };
 
-    return () => {
-      // Eliminar los event listeners solo si los elementos existen
-      signUpButton?.removeEventListener("click", handleSignUpClick);
-      signInButton?.removeEventListener("click", handleSignInClick);
-    };
-  }, []);
+  const handleRegistration = () => {
+    // Tu lógica de registro aquí
+
+    // Después del registro exitoso, redirige a la página principal
+    router.push("/filesUpload");
+  };
 
   return (
-    <div className="container" ref={containerRef}>
+    <div className={`container ${isSignUp ? "right-panel-active" : ""}`} ref={containerRef}>
       <div className="form-container sign-up-container">
-        <form action="#">
+        <form action="filesUpload">
           <h1>Crear Cuenta</h1>
           <div className="social-container">
             <a href="#" className="social">
@@ -50,15 +52,17 @@ export default function Login() {
               <i className="fab fa-linkedin-in"></i>
             </a>
           </div>
-          <span>o utiliza tu correo electrónico para registrarte</span>
+          <span>o usa tu correo electrónico para registrarte</span>
           <input type="text" placeholder="Nombre" />
-          <input type="email" placeholder="Correo Electrónico" />
+          <input type="text" placeholder="Correo electrónico" />
           <input type="password" placeholder="Contraseña" />
-          <button>Registrarse</button>
+          <button onClick={handleRegistration}>
+            Registrarse
+          </button>
         </form>
       </div>
       <div className="form-container sign-in-container">
-        <form action="#">
+        <form action="filesUpload">
           <h1>Iniciar Sesión</h1>
           <div className="social-container">
             <a href="#" className="social">
@@ -71,28 +75,28 @@ export default function Login() {
               <i className="fab fa-linkedin-in"></i>
             </a>
           </div>
-          <span>o utiliza tu cuenta</span>
-          <input type="email" placeholder="Correo Electrónico" />
+          <span>o usa tu cuenta</span>
+          <input type="text" placeholder="Correo electrónico" />
           <input type="password" placeholder="Contraseña" />
           <a href="#">¿Olvidaste tu contraseña?</a>
-          <button>Iniciar Sesión</button>
+          <button onClick={handleSignIn}>
+            Iniciar Sesión
+          </button>
         </form>
       </div>
       <div className="overlay-container">
         <div className="overlay">
-          <div className="overlay-panel overlay-left">
-            <h1>¡Bienvenido de nuevo!</h1>
-            <p>
-              Para mantenerte conectado con nosotros, inicia sesión con tu información personal.
-            </p>
-            <button className="ghost" ref={signInButtonRef}>
+          <div className={`overlay-panel overlay-left ${isSignUp ? "" : "right-panel-active"}`}>
+            <h2>¡Bienvenido de nuevo!</h2>
+            <p>Para estar conectado con nosotros, inicia sesión con tu información personal</p>
+            <button className="ghost" ref={signInButtonRef} onClick={handleSignInClick}>
               Iniciar Sesión
             </button>
           </div>
-          <div className="overlay-panel overlay-right">
-            <h1>¡Hola, Amigo!</h1>
+          <div className={`overlay-panel overlay-right ${isSignUp ? "right-panel-active" : ""}`}>
+            <h2>¡Hola, Amigo!</h2>
             <p>Ingresa tus detalles personales y comienza tu viaje con nosotros</p>
-            <button className="ghost" ref={signUpButtonRef}>
+            <button className="ghost" ref={signUpButtonRef} onClick={handleSignUpClick}>
               Registrarse
             </button>
           </div>
